ec9e47ea84d852f96c0b84657f048dce1811499b
Ben Good <benjamingood@google.com>
2021-09-02 13:10:01 -0600

    Simplifying the logger initilization and wrappers.

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -15,68 +15,45 @@
 package logger
 
 import (
-	"fmt"
 	"log"
 	"os"
 
 	"go.uber.org/zap"
-	"go.uber.org/zap/zapcore"
 )
 
 var env = os.Getenv("ACTIVE_ENV")
 var logger *zap.SugaredLogger
-var errorLogger *zap.SugaredLogger
 
 // init - this will init logger in the project
 func init() {
-	devConfig := zap.NewDevelopmentConfig()
-	devConfig.DisableStacktrace = true
-	w := LogWriter{}
-	tmp, err := devConfig.Build(zap.AddCallerSkip(1), zap.WrapCore(func(zapcore.Core) zapcore.Core {
-		return zapcore.NewCore(zapcore.NewJSONEncoder(devConfig.EncoderConfig), zapcore.AddSync(w), devConfig.Level)
-	}))
+	config := zap.NewProductionConfig()
+	config.DisableStacktrace = true
+
+	if env != "PRODUCTION" {
+		config = zap.NewDevelopmentConfig()
+	}
+
+	tmp, err := config.Build()
 	if err != nil {
 		log.Fatal(err)
 	}
-
 	logger = tmp.Sugar()
-
-	prodLogger := zap.NewProductionConfig()
-	prodLogger.DisableStacktrace = true
-	tempProd, err := prodLogger.Build(zap.AddCallerSkip(2), zap.WrapCore(func(zapcore.Core) zapcore.Core {
-		return zapcore.NewCore(zapcore.NewJSONEncoder(zap.NewProductionEncoderConfig()), zapcore.AddSync(w), devConfig.Level)
-	}))
-	if err != nil {
-		log.Fatal(err)
-	}
-	errorLogger = tempProd.Sugar()
-	defer errorLogger.Sync()
 	defer logger.Sync()
 }
 
 // LogError - This is error level log
 func LogError(message ...interface{}) {
-	if env != "PRODUCTION" {
-		logger.Error(message)
-	} else {
-		errorLogger.Error(message)
-	}
+	logger.Error(message)
 }
 
 // LogErrorF - This is error level log
 func LogErrorF(format string, message ...interface{}) {
-	if env != "PRODUCTION" {
-		logger.Errorf(format, message...)
-	} else {
-		errorLogger.Errorf(format, message...)
-	}
+	logger.Errorf(format, message...)
 }
 
 // LogInfo - This is Info level log
 func LogInfo(message ...interface{}) {
-	if env != "PRODUCTION" {
-		logger.Info(message)
-	}
+	logger.Info(message)
 }
 
 // LogWarn - This is Warn level log
@@ -86,29 +63,15 @@ func LogWarn(message ...interface{}) {
 
 // LogDebug - This is debug level log
 func LogDebug(message ...interface{}) {
-	if env != "PRODUCTION" {
-		logger.Debug(message)
-	}
+	logger.Debug(message)
 }
 
 // LogFatal - This log error and fatal it
 func LogFatal(message ...interface{}) {
-	errorLogger.Fatal(message)
+	logger.Fatal(message)
 }
 
 // ErrorLogging - This log error and fatal it
 func ErrorLogging(message ...interface{}) {
-	errorLogger.Error(message)
-}
-
-// LogWriter - Log Writer
-type LogWriter struct{}
-
-func (m LogWriter) Write(ba []byte) (int, error) {
-	if env == "PRODUCTION" {
-		go fmt.Println(string(ba))
-	} else {
-		fmt.Println(string(ba))
-	}
-	return len(ba), nil
+	logger.Error(message)
 }
