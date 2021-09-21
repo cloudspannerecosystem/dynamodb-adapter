e61eb4da03670c0cf117c554aa04646856ed3fc3
Benjamin Good <benjamingood@google.com>
2021-09-21 14:20:12 -0600

    Example application written in Golang (#27)
    * Initial commit of example data, scripts and golang application.
    
    * Added more read access patterns
    
    * Pulling region from env vars.
    
    * Updates to handle the query endpoints for the adatper.
    
    * Adding config for the indexes.
    
    * Removing the code to make extra sessions.
    
    * Updating to go 1.17
    
    * Cleaning up the README some and updating to the new config file names.
    
    * Found a couple more places were config file names needed to be fixed.
    
    * Apply suggestions from code review
    
    Co-authored-by: skuruppu <skuruppu@google.com>
    
    Co-authored-by: skuruppu <skuruppu@google.com>

---
+++ examples/golang/main.go
@@ -0,0 +1,286 @@
+// Copyright 2021 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package config implements the functions for reading
+// configuration files and saving them into Golang objects
+
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/aws/aws-sdk-go/aws"
+
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+)
+
+func main() {
+	var sess *session.Session
+
+	switch cmd := os.Args[1]; cmd {
+	case "spanner":
+		sess = createAdapterSession()
+	case "dynamo":
+		sess = createSession("")
+	}
+
+	svc := dynamodb.New(sess)
+
+	fmt.Println("Dynamo GetItem:")
+	getCustomerContactDetails(svc)
+
+	fmt.Println("\nDynamo PK/SK Query:")
+	getCustomerOrderDetails(svc)
+
+	fmt.Println("\nDynamo Index Query:")
+	getCustomerMostRecentOrder(svc)
+
+	fmt.Println("\nDynamo PK Query:")
+	getOrderLineItemDetails(svc)
+
+	fmt.Println("\nDynamo Index Query")
+	getProductsByCategory(svc)
+}
+
+func getCustomerContactDetails(svc *dynamodb.DynamoDB) {
+	result, err := svc.GetItem(&dynamodb.GetItemInput{
+		TableName: aws.String("Customer_Order"),
+		Key: map[string]*dynamodb.AttributeValue{
+			"PK": {
+				S: aws.String("CUST#0000000000"),
+			},
+			"SK": {
+				S: aws.String("EMAIL#homer@email.com"),
+			},
+		},
+	})
+
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	if result.Item == nil {
+		fmt.Printf("No record found")
+	}
+
+	customer := Customer{}
+	err = dynamodbattribute.UnmarshalMap(result.Item, &customer)
+	if err != nil {
+		panic(fmt.Sprintf("Failed to unmarshal Record, %v", err))
+	}
+
+	printCustomer(customer)
+}
+
+func getCustomerOrderDetails(svc *dynamodb.DynamoDB) {
+	result, err := svc.Query(&dynamodb.QueryInput{
+		TableName:              aws.String("Customer_Order"),
+		KeyConditionExpression: aws.String("PK = :customer_id and SK = :order_id"),
+		ExpressionAttributeValues: map[string]*dynamodb.AttributeValue{
+			":customer_id": {
+				S: aws.String("CUST#0000000000"),
+			},
+			":order_id": {
+				S: aws.String("ORDER#ej68vuldzgps"),
+			},
+		},
+	})
+
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	if result.Items == nil {
+		fmt.Printf("No record found")
+	}
+
+	customer := Customer{}
+	err = dynamodbattribute.UnmarshalMap(result.Items[0], &customer)
+	if err != nil {
+		panic(fmt.Sprintf("Failed to unmarshal Record, %v", err))
+	}
+
+	order := Order{}
+	err = dynamodbattribute.UnmarshalMap(result.Items[0], &order)
+	if err != nil {
+		panic(fmt.Sprintf("Failed to unmarshal Record, %v", err))
+	}
+
+	printCustomer(customer)
+	printOrder(order)
+}
+
+func getCustomerMostRecentOrder(svc *dynamodb.DynamoDB) {
+	result, err := svc.Query(&dynamodb.QueryInput{
+		TableName:              aws.String("Customer_Order"),
+		IndexName:              aws.String("By_customer"),
+		KeyConditionExpression: aws.String("customer_id = :customer_id AND order_ts BETWEEN :order_ts_start AND :order_ts_end"),
+		ExpressionAttributeValues: map[string]*dynamodb.AttributeValue{
+			":customer_id": {
+				S: aws.String("0000000000"),
+			},
+			":order_ts_start": {
+				S: aws.String("2021-05-01T00:00:00.000000"),
+			},
+			":order_ts_end": {
+				S: aws.String("2021-05-31T00:00:00.000000"),
+			},
+		},
+	})
+
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	if result.Items == nil {
+		fmt.Printf("No record found")
+	}
+
+	customer := Customer{}
+	err = dynamodbattribute.UnmarshalMap(result.Items[0], &customer)
+	if err != nil {
+		panic(fmt.Sprintf("Failed to unmarshal Record, %v", err))
+	}
+	printCustomer(customer)
+
+	for _, item := range result.Items {
+		order := Order{}
+		err = dynamodbattribute.UnmarshalMap(item, &order)
+		if err != nil {
+			panic(fmt.Sprintf("Failed to unmarshal Record, %v", err))
+		}
+
+		printOrder(order)
+	}
+}
+
+func getOrderLineItemDetails(svc *dynamodb.DynamoDB) {
+	result, err := svc.Query(&dynamodb.QueryInput{
+		TableName:              aws.String("Customer_Order"),
+		KeyConditionExpression: aws.String("PK = :order_id"),
+		ExpressionAttributeValues: map[string]*dynamodb.AttributeValue{
+			":order_id": {
+				S: aws.String("ORDER#ej68vuldzgps"),
+			},
+		},
+	})
+
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	if result.Items == nil {
+		fmt.Printf("No record found")
+	}
+
+	order := Order{}
+	err = dynamodbattribute.UnmarshalMap(result.Items[0], &order)
+	if err != nil {
+		panic(fmt.Sprintf("Failed to unmarshal Record, %v", err))
+	}
+	printOrder(order)
+
+	for _, item := range result.Items {
+		lineItem := LineItem{}
+		err = dynamodbattribute.UnmarshalMap(item, &lineItem)
+		if err != nil {
+			panic(fmt.Sprintf("Failed to unmarshal Record, %v", err))
+		}
+		printLineItem(lineItem)
+	}
+}
+
+func getProductsByCategory(svc *dynamodb.DynamoDB) {
+	result, err := svc.Query(&dynamodb.QueryInput{
+		TableName:              aws.String("Product"),
+		IndexName:              aws.String("By_Product_Category"),
+		KeyConditionExpression: aws.String("product_category = :product_category"),
+		ExpressionAttributeValues: map[string]*dynamodb.AttributeValue{
+			":product_category": {
+				S: aws.String("HardGood#Musical Instruments#Drums & Percussion"),
+			},
+		},
+	})
+
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	if result.Items == nil {
+		fmt.Printf("No record found")
+	}
+
+	for _, item := range result.Items {
+		electronic := Electronic{}
+		err = dynamodbattribute.UnmarshalMap(item, &electronic)
+		if err != nil {
+			panic(fmt.Sprintf("Failed to unmarshal Record, %v", err))
+		}
+		printElectronic(electronic)
+	}
+}
+
+func printCustomer(c Customer) {
+	fmt.Println("Customer Info:")
+	fmt.Println("\tId:        ", c.Id)
+	fmt.Println("\tEmail:     ", c.Email)
+	fmt.Println("\tFname:     ", c.Fname)
+	fmt.Println("\tLname:     ", c.Lname)
+	fmt.Println("\tAddresses: ", c.Addresses)
+}
+
+func printOrder(o Order) {
+	fmt.Println("Order details:")
+	fmt.Println("\tId:         ", o.Id)
+	fmt.Println("\tStatus:     ", o.Status)
+	fmt.Println("\tAmount:     ", o.Amount)
+	fmt.Println("\tItem Count: ", o.NumberOfItems)
+	fmt.Println("\tOrder Time: ", o.Ts)
+}
+
+func printLineItem(i LineItem) {
+	fmt.Println("LineItem details:")
+	fmt.Println("\tId:         ", i.Id)
+	fmt.Println("\tProduct Id: ", i.ProductId)
+	fmt.Println("\tPrice:      ", i.Price)
+	fmt.Println("\tDiscount:   ", i.Discount)
+	fmt.Println("\tStatus:     ", i.Status)
+	fmt.Println("\tComment:    ", i.Comment)
+}
+
+func printElectronic(e Electronic) {
+	fmt.Println("Electronic details:")
+	fmt.Println("\tId:       ", e.Id)
+	fmt.Println("\tName:     ", e.Name)
+	fmt.Println("\tCategory: ", e.Category)
+	fmt.Println("\tModel:    ", e.Model)
+	fmt.Println("\tPrice:    ", e.Price)
+	fmt.Println("\tShipping: ", e.ShippingAmount)
+}
+
+var region = os.Getenv("AWS_REGION")
+
+func createSession(url string) *session.Session {
+	if url == "" {
+		url = "https://dynamodb." + region + ".amazonaws.com"
+	}
+
+	return session.Must(session.NewSessionWithOptions(session.Options{
+		Config: aws.Config{
+			Endpoint: aws.String(url),
+		},
+	}))
+}
+
+func createAdapterSession() *session.Session {
+	return createSession("http://localhost:9050/v1")
+}
--- go.mod
+++ go.mod
@@ -1,35 +1,93 @@
 module github.com/cloudspannerecosystem/dynamodb-adapter
 
-go 1.13
+go 1.17
 
 require (
-	cloud.google.com/go/pubsub v1.5.0
-	cloud.google.com/go/spanner v1.7.0
-	github.com/GeertJohan/go.rice v1.0.0
+	cloud.google.com/go/pubsub v1.17.0
+	cloud.google.com/go/spanner v1.25.0
+	github.com/GeertJohan/go.rice v1.0.2
 	github.com/ahmetb/go-linq v3.0.0+incompatible
 	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751
-	github.com/antonmedv/expr v1.8.8
-	github.com/aws/aws-sdk-go v1.33.5
+	github.com/antonmedv/expr v1.9.0
+	github.com/aws/aws-sdk-go v1.40.43
 	github.com/fatih/structs v1.1.0 // indirect
-	github.com/gavv/httpexpect v2.0.0+incompatible
 	github.com/gavv/httpexpect/v2 v2.1.0
 	github.com/gin-contrib/pprof v1.3.0
-	github.com/gin-gonic/gin v1.6.3
+	github.com/gin-gonic/gin v1.7.4
 	github.com/gorilla/websocket v1.4.2 // indirect
 	github.com/imkira/go-interpol v1.1.0 // indirect
-	github.com/moul/http2curl v1.0.0 // indirect
 	github.com/opentracing/opentracing-go v1.2.0
 	github.com/robfig/cron v1.2.0
 	github.com/satori/go.uuid v1.2.0
-	github.com/stretchr/testify v1.5.1
-	github.com/swaggo/gin-swagger v1.2.0
-	github.com/swaggo/swag v1.6.7
-	github.com/tidwall/gjson v1.6.0
+	github.com/stretchr/testify v1.7.0
+	github.com/swaggo/gin-swagger v1.3.1
+	github.com/swaggo/swag v1.7.1
+	github.com/tidwall/gjson v1.9.0
 	github.com/valyala/fasthttp v1.15.1 // indirect
 	github.com/xeipuuv/gojsonschema v1.2.0 // indirect
-	go.uber.org/zap v1.15.0
-	golang.org/x/tools v0.0.0-20201117021029-3c3a81204b10 // indirect
-	google.golang.org/api v0.29.0
-	google.golang.org/genproto v0.0.0-20200711021454-869866162049
+	go.uber.org/zap v1.19.1
+	golang.org/x/tools v0.1.5 // indirect
+	google.golang.org/api v0.56.0
+	google.golang.org/genproto v0.0.0-20210909211513-a8c4777a87af
 	gopkg.in/go-playground/assert.v1 v1.2.1
+)
+
+require (
+	cloud.google.com/go v0.94.1 // indirect
+	github.com/KyleBanks/depth v1.2.1 // indirect
+	github.com/PuerkitoBio/purell v1.1.1 // indirect
+	github.com/PuerkitoBio/urlesc v0.0.0-20170810143723-de5bf2ad4578 // indirect
+	github.com/ajg/form v1.5.1 // indirect
+	github.com/andybalholm/brotli v1.0.0 // indirect
+	github.com/daaku/go.zipexe v1.0.1 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/gin-contrib/sse v0.1.0 // indirect
+	github.com/go-openapi/jsonpointer v0.19.5 // indirect
+	github.com/go-openapi/jsonreference v0.19.6 // indirect
+	github.com/go-openapi/spec v0.20.3 // indirect
+	github.com/go-openapi/swag v0.19.15 // indirect
+	github.com/go-playground/locales v0.14.0 // indirect
+	github.com/go-playground/universal-translator v0.18.0 // indirect
+	github.com/go-playground/validator/v10 v10.9.0 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/go-cmp v0.5.6 // indirect
+	github.com/google/go-querystring v1.0.0 // indirect
+	github.com/googleapis/gax-go/v2 v2.1.0 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/klauspost/compress v1.10.7 // indirect
+	github.com/leodido/go-urn v1.2.1 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/sergi/go-diff v1.0.0 // indirect
+	github.com/tidwall/match v1.0.3 // indirect
+	github.com/tidwall/pretty v1.2.0 // indirect
+	github.com/ugorji/go/codec v1.2.6 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/xeipuuv/gojsonpointer v0.0.0-20180127040702-4e3ac2762d5f // indirect
+	github.com/xeipuuv/gojsonreference v0.0.0-20180127040603-bd5ef7bd5415 // indirect
+	github.com/yalp/jsonpath v0.0.0-20180802001716-5cc68e5049a0 // indirect
+	github.com/yudai/gojsondiff v1.0.0 // indirect
+	github.com/yudai/golcs v0.0.0-20170316035057-ecda9a501e82 // indirect
+	go.opencensus.io v0.23.0 // indirect
+	go.uber.org/atomic v1.9.0 // indirect
+	go.uber.org/multierr v1.7.0 // indirect
+	golang.org/x/crypto v0.0.0-20210711020723-a769d52b0f97 // indirect
+	golang.org/x/net v0.0.0-20210614182718-04defd469f4e // indirect
+	golang.org/x/oauth2 v0.0.0-20210819190943-2bc19b11175f // indirect
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
+	golang.org/x/sys v0.0.0-20210823070655-63515b42dcdf // indirect
+	golang.org/x/text v0.3.6 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+	google.golang.org/appengine v1.6.7 // indirect
+	google.golang.org/grpc v1.40.0 // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
+	moul.io/http2curl v1.0.1-0.20190925090545-5cd742060b0e // indirect
 )
