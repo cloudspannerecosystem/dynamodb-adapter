c75f73bf3d280dc21239b26add0972889e8b1b37
Taher Lakdawala <78196491+taherkl@users.noreply.github.com>
2025-02-26 09:30:06 +0530

    Null DataType Support (#84)
    * Null DataType Support (#33)
    
    
    Co-authored-by: Nikita Jain <nikitajain1998@gmail.com>
    Co-authored-by: taherkl <taher.lakdawala@ollion.com>

--- samples/main.go
+++ samples/main.go
@@ -45,6 +45,9 @@ func createItem(svc *dynamodb.DynamoDB) {
 		"emp_image": {
 			B: []byte("binary_data_here"),
 		},
+		"emp_status": {
+			NULL: aws.Bool(true), // Explicitly setting NULL
+		},
 		"emp_details": {
 			S: aws.String(empDetailsJSON),
 		},
